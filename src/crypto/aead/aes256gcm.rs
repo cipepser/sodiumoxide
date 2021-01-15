0c4952a11e24bc319898883ccb06f6d1a945c41e
cipepser <respepic@gmail.com>
2021-01-15 18:32:02 +0900

    fix an import error

--- src/crypto/aead/aes256gcm.rs
+++ src/crypto/aead/aes256gcm.rs
@@ -81,6 +81,11 @@ mod aes_api {
     use super::aes_impl;
     use crypto::nonce::gen_random_nonce;
 
+    #[cfg(not(feature = "std"))]
+    use prelude::*;
+    #[cfg(eature = "std")]
+    use std::vec::Vec;
+
     /// The Aes256Gcm struct encapsulates the crypto_aead_aes256gcm_* family of
     /// functions in a way that ensures safe usage of the API at runtime
     /// without incurring a per function call cost.
